b54c7136d7c958efc222e99daacfcd63a0e420d0
hamzahamruni <hamzahamroni95@gmail.com>
2024-01-13 14:37:22 +0200

    24-01-13

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //______________________________________
 const express = require("express")
+const cors = require('cors')
+
 const app = express();
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
+const { tr } = require("date-fns/locale");
 //const {Client,LocalAuth} = require("whatsapp-web.js")
 //const port =3001;
 //______________________________________
@@ -15,6 +18,8 @@ const { Client } = require('whatsapp-web.js');
 //         clientId:"YOUR_CLIENT_ID"
 //     })
 // });
+app.use(cors());
+app.use(express.json());
 const client = new Client();
 //______________________________________
 client.on('qr', (qr) => {
@@ -23,32 +28,48 @@ client.on('qr', (qr) => {
 //______________________________________
 client.on('ready',()=>{
     console.log("client ready");
-    sendmsg()
-})
-async function sendmsg(){
-    const numbers =
-    [
-       {
-        number: "911234567",
-        msg :  "msg"
-       }
-    ];// add phone numbers you want send message
-    numbers.map( async (index)=>{
-        var sanitized_number = index.number.toString().replace(/[- )(]/g, ""); // remove unnecessary chars from the number
-        var final_number = `218${sanitized_number.substring(sanitized_number.length - 10)}`; // add 218 before the number here 91 is country code of Libya
-        console.log("final_number = "+final_number);
-        var number_details = await client.getNumberId(final_number); // get mobile number details
 
-        if (number_details) {
-            var sendMessageData = await client.sendMessage(number_details._serialized, index.msg); // send message
-            console.log(final_number)
-        }
-        else
+})
+app.get('/whatsapp-msg/:number',async function sendmsg(req,res){
+    try{
+        const {number} = req.params
+        const numbers =
+        [
         {
-            console.log(final_number, "Mobile number is not registered");
+            number: number,
+            msg :  "YOUR MSG"
         }
-    });
-}
+        ];// add phone numbers you want send message
+        numbers.map( async (index)=>{
+            var sanitized_number = index.number.toString().replace(/[- )(]/g, ""); // remove unnecessary chars from the number
+            var final_number = `218${sanitized_number.substring(sanitized_number.length - 10)}`; // add 218 before the number here 91 is country code of Libya
+            console.log("final_number = "+final_number);
+            var number_details = await client.getNumberId(final_number); // get mobile number details
+
+            if (number_details) {
+                var sendMessageData = await client.sendMessage(number_details._serialized, index.msg); // send message
+                console.log(final_number)
+                return res.json({
+                    success:1
+                })
+            }
+            else
+            {
+                console.log(final_number, "Mobile number is not registered");
+                return res.json({
+                    success:0
+                })
+            }
+        });
+    }
+    catch(error)
+    {
+       console.log(error);
+    }
+})
+
 //______________________________________
 client.initialize();
+const port = 4001
+app.listen(port, () => console.log(`🟢 app listening on port ${port}!`))
 
